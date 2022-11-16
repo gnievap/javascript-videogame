d29b4b2adfd1ef0c14cbd678fdcda02e5578c8e2
Gaby Nieva <gaby.nieva@outlook.com>
2022-11-16 15:59:41 -0600

    Comenzando a usar Canvas

--- game.js
+++ game.js
@@ -1,1 +1,18 @@
-console.log(maps);
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+window.addEventListener('load', startGame);
+
+function startGame(){
+    // función para inicializar al inicio del juego
+
+    // dibujar un rectangulo: x1, y1, ancho, altura
+    game.fillRect(0, 0, 100, 100);
+    // borra utilizando un rectángulo
+    game.clearRect(50, 50, 50, 50);
+    game.font = '25px Verdana';
+    game.fillStyle = 'purple';
+    game.textAlign = 'center';
+    game.fillText('Platzi', 100, 100);
+    
+}
