fea2f8167b475f82bfb8d7c0bad4ee43f82624fa
Gaby Nieva <gaby.nieva@outlook.com>
2022-11-16 23:45:08 -0600

    Tamaño del canvas y de los elementos del juego

--- game.js
+++ game.js
@@ -6,6 +6,30 @@ window.addEventListener('load', startGame);
 function startGame(){
     // función para inicializar al inicio del juego
 
+    // Definir el tamaño del canvas
+    let canvasSize;
+
+    if ( window.innerHeight > window.innerWidth ){
+        canvasSize = window.innerWidth * 0.8;
+    } else {
+        canvasSize = window.innerHeight * 0.8;
+    }
+
+    canvas.setAttribute('width',canvasSize);
+    canvas.setAttribute('height', canvasSize);
+
+    const elementsSize = (canvasSize / 10) - 1;
+
+    game.font = elementsSize + 'px Verdana';
+    game.textAlign = 'end';
+    
+    for (let i = 1; i <= 10; i++){
+        game.fillText(emojis['X'], elementsSize * i, elementsSize);
+    }
+    
+
+
+    /*
     // dibujar un rectangulo: x1, y1, ancho, altura
     game.fillRect(0, 0, 100, 100);
     // borra utilizando un rectángulo
@@ -13,6 +37,6 @@ function startGame(){
     game.font = '25px Verdana';
     game.fillStyle = 'purple';
     game.textAlign = 'center';
-    game.fillText('Platzi', 100, 100);
+    game.fillText('Platzi', 100, 100);*/
     
 }
