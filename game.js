4329f78108f7a808c25d5a92f8c61dd123e4dac6
Gaby Nieva <gaby.nieva@outlook.com>
2022-11-20 21:03:43 -0600

    Intro a los arreglos multidimensionales

--- game.js
+++ game.js
@@ -30,6 +30,8 @@ function startGame(){
     game.textAlign = 'center';
     
     for (let i = 1; i <= 10; i++){
-            game.fillText(emojis['X'], elementsSize, elementsSize * i);
+        for ( let j = 1; j <= 10; j++ ){
+            game.fillText(emojis['X'], elementsSize * i, elementsSize * j);
+        }   
     }
 }
