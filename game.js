2babba86cc9ecd6bd84dc5cd79a6eb21ad7e8920
Gaby Nieva <gaby.nieva@outlook.com>
2022-11-22 10:59:54 -0600

    Asignando eventos a botones y teclas

--- game.js
+++ game.js
@@ -1,6 +1,12 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
+const btnUp = document.querySelector('#up');
+const btnLeft = document.querySelector('#left');
+const btnRight = document.querySelector('#right');
+const btnDown = document.querySelector('#down');
+
+
 let canvasSize;
 let elementsSize;
 
@@ -40,4 +46,36 @@ function startGame(){
             game.fillText(emojis[mapRowCols[row - 1][col - 1]], elementsSize * col, elementsSize * row);
         }   
     }
+}
+
+window.addEventListener('keydown', moveByKeys);
+
+btnUp.addEventListener('click', moveUp);
+btnLeft.addEventListener('click', moveLeft);
+btnRight.addEventListener('click', moveRight);
+btnDown.addEventListener('click', moveDown);
+
+
+function moveByKeys(event){
+   // console.log(event);
+   if ( event.key == 'ArrowUp') moveUp();
+   else if (event.key == 'ArrowLeft') moveLeft();
+   else if (event.key == 'ArrowRight') moveRight();
+   else if (event.key == 'ArrowDown') moveDown();
+}
+
+function moveUp(){
+    console.log('Arriba');
+}
+
+function moveLeft(){
+    console.log('Izquierda');
+}
+
+function moveRight(){
+    console.log('Derecha');
+}
+
+function moveDown(){
+    console.log('Abajo');
 }
